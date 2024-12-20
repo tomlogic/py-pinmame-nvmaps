524b04bdea64f13474e089bf8662b219a7401e1a
Tom Collins <tom@tomlogic.com>
2024-12-20 10:07:20 -0500

    test: add bare-bones testing
    Uses .nv files from https://github.com/francisdb/pinmame-nvram
    No code to validate results, but can detect changes in output.

---
+++ test/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+(
+  cd "$SCRIPTPATH"
+  mkdir -p results
+  rm results/*.txt
+  for file in nvram/*.nv; do
+    filename=$(basename "$file")
+    python3 ../nvram_parser.py --nvram "$file" --dump > "results/$filename.txt" 2>&1
+  done
+  diff --unified --recursive --paginate expected results | more
+)
